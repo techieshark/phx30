9e151cb34fea93a4389043e936e9695158390d8f
Peter W <techieshark@gmail.com>
2014-03-17 02:44:28 -0700

    process 25 destinatios and sort them
    - uses lo-dash (underscore superset) for some functional abilities.

--- main.js
+++ main.js
@@ -1,48 +1,119 @@
-var g; //global var for debugging
+// debugging globals
+var g_locs; // locations
+var g_resp; // response
+var g_geojson;
+var g_foo, g_bar;
 
-$(document).ready(function() {
+// A handy sort function and a couple helpers
+function sortByProp (propertyRetriever, arr) {
+    arr.sort(function (a, b) {
+        var valueA = propertyRetriever(a);
+        var valueB = propertyRetriever(b);
 
-    console.log("eeeehlllooo!");
-    var origins      = ["33.4150,-111.8314"];
-    var destinations = ["33.3213094,-111.9116732"];
+        if (valueA < valueB) {
+            return -1;
+        } else if (valueA > valueB) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+}
+function distanceRetriever(obj) {
+    return obj.distance.value;
+}
+function durationRetriever(obj) {
+    return obj.duration.value;
+}
+
+// STUB
+function getCurrentLocation() {
+    return "33.4150,-111.8314";
+}
+
+// Given a GeoJSON of locations, find and merge in the distance & durations to each
+// then call callback when done.
+function distanceTo(locations, callback) {
+    var origins      = [getCurrentLocation()];
+    var destinations = _.map(locations, function(location) { return location.lat + "," + location.lon });
 
     var distanceMatrix  = new google.maps.DistanceMatrixService();
-    var distanceRequest = { origins: origins, destinations: destinations, travelMode: google.maps.TravelMode.DRIVING, unitSystem: google.maps.UnitSystem.IMPERIAL, avoidHighways: false, avoidTolls: false };
+
+    var distanceRequest = {
+        origins: origins,
+        destinations: destinations,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+        avoidHighways: false,
+        avoidTolls: false
+    };
 
     distanceMatrix.getDistanceMatrix(distanceRequest, function(response, status) {
         if (status != google.maps.DistanceMatrixStatus.OK) {
             alert('Error was: ' + status);
         }
         else {
-            g = response;
-            var origins      = response.originAddresses;
-            var destinations = response.destinationAddresses;
-            // rest of your code here...
-            console.log(origins);
-            console.log(destinations);
+            g_resp = response; // DEBUGGING
+            // var origins      = response.originAddresses;
+            // var destinations = response.destinationAddresses;
 
-            console.log(response);
+            // console.log("Distance: " + response.rows[0].elements[0].distance.text + " (" + response.rows[0].elements[0].duration.text + ")");
 
-            console.log("Distance: " + response.rows[0].elements[0].distance.text + " (" + response.rows[0].elements[0].duration.text + ")");
+            _.merge(locations, response.rows[0].elements);
+
+            callback();
         }
     });
+}
+
+
+
+$(document).ready(function() {
+
+    $.getJSON("places.geojson", function(json) {
+        console.log(json);
+        g_geojson = json; // DEBUG
+
+        // Google Distance Matrix API lets us look up a 1 x 25 matrix of distances (1 origin, 25 destinations)
+        // so we need to buid an array like [ (lat,lon)... ]
+        // TODO: pull in more than 25
+        var first25 = json.features.slice(0,25);
+        var locations = g_locs = _.map(first25, function(f) {
+            console.log("Found a " + f.properties.amenity + " (" + f.properties.name + ") at " + f.geometry.coordinates[1] + "," + f.geometry.coordinates[0] );
+            return {
+                "amenity": f.properties.amenity,
+                "name": f.properties.name,
+                "lat": f.geometry.coordinates[1], "lon": f.geometry.coordinates[0]
+            };
+        });
+
+        distanceTo(locations, function() {
+
+            //g_bar = sortByProp(distanceRetriever, locations);
+            g_foo = sortByProp(durationRetriever, locations);
+            // list: [ place, amenity, lat/lon, distance, duration, ]
+
+            // display a list something like:
+            // You're bored? Maybe you should check out...
+            // The Mesa Arts Center (13 minutes away)
+            // HeatSync Labs (14 minutes away)
+            //
+            // we get name of place and amenity type from geoJSON.
+            // we get distance from google.
+            //
+            console.log("Bored? Maybe you should: ");
+
+            for (var i = 0; i < locations.length; i++) {
+                loc = locations[i];
+                var start;
+                if (loc.amenity) {
+                    start = "Go to a " + loc.amenity + " ("+loc.name+"). ";
+                } else {
+                    start = "Go to " + loc.name + ". ";
+                }
+                console.log(start + "It is only " + loc.duration.text + " (" + loc.distance.text + ") away.");
+            }
+
+        });
+    });
 });
-
-// var ORIGIN = "33.4150,-111.8314";
-// var DESTINATION = "33.3213094,-111.9116732";
-
-// function httpGet(theUrl)
-// {
-//   var xmlHttp = null;
-
-//   xmlHttp = new XMLHttpRequest();
-//   xmlHttp.open( "GET", theUrl, false );
-//   xmlHttp.send( null );
-//   return xmlHttp.responseText;
-// }
-
-// var url = "http://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyAzOF-EGWefl40wZ28RNwZLG4MRVQCw6cg&origins=" + ORIGIN + "&destinations=" + DESTINATION + "&sensor=true";
-
-// var response = httpGet(url);
-
-// console.debug("%o", response);
